c28223523d0a0bdf7efe114e16d83d89ffd93ef9
Stormpero <staralex.const@gmail.com>
2023-01-15 12:44:12 +0300

    init

---
+++ Paths.py
@@ -0,0 +1,30 @@
+import configparser, os
+
+CONFIG_FILENAME = "paths.ini"
+
+
+# self.PATH_INPUT = r"F:\Stormpero\Сборка Маинкравт\bundles"
+# self.PATH_OUTPUT = r"C:\Users\staro\AppData\Roaming\.minecraft\Another Versions\Automation Technology\mods"
+# self.PATH_MINECRAFT = r"C:\Users\staro\AppData\Roaming\.minecraft\Minecraft.lnk"
+
+class Paths:
+
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        if not os.path.exists(CONFIG_FILENAME):
+            self.config["PATH"] = {
+                "PATH_INPUT": "",
+                "PATH_OUTPUT": "",
+                "PATH_MINECRAFT": ""
+            }
+            self.save()
+        else:
+            self.config.read(CONFIG_FILENAME)
+
+    def get(self):
+        return self.config["PATH"]
+
+    def save(self):
+        with open(CONFIG_FILENAME, 'w') as configfile:
+            self.config.write(configfile)
+
---
+++ main.py
@@ -0,0 +1,158 @@
+import os
+import shutil
+import time
+import subprocess
+
+import tkinter as tk
+from tkinter import filedialog
+
+import pyautogui
+import PIL.Image
+from pystray import Menu, MenuItem, Icon
+
+from Paths import Paths
+
+root = tk.Tk()
+root.withdraw()
+
+paths = Paths()
+paths_config = paths.get()
+
+
+def clearFolderMods():
+    shutil.rmtree(paths_config["PATH_OUTPUT"])
+    os.makedirs(paths_config["PATH_OUTPUT"])
+
+
+def startMinecraft():
+    os.system(f"start {paths_config['PATH_MINECRAFT']}")
+    x = 970
+    y = 745
+    color = "(0, 140, 69)"
+    while True:
+        px = pyautogui.pixel(x, y)
+        print(str(px))
+        if str(px) == color:
+            pyautogui.click(x, y)
+            break
+        time.sleep(0.5)
+
+
+def updateModes():
+    clearFolderMods()
+    for root, subFolder, files in os.walk(paths_config["PATH_INPUT"]):
+        for item in files:
+            if item.endswith(".jar"):
+                print(root + "\\" + item)
+                shutil.copy(root + "\\" + item, paths_config["PATH_OUTPUT"] + "\\" + item)
+
+
+def updateModesAndStart():
+    updateModes()
+    startMinecraft()
+
+
+image_main = PIL.Image.open("icons/buildmods.ico")
+image_loading = PIL.Image.open("icons/loading.ico")
+image_error = PIL.Image.open("icons/error.ico")
+
+
+def is_loading(icon, state):
+    icon.icon = image_loading if state else image_main
+
+
+def on_ready(icon):
+    icon.notify('Ready')
+
+
+def is_filepaths_exists(icon):
+    if not paths_config["PATH_INPUT"]:
+        icon.notify('PATH_INPUT is None. Enter it in settings!')
+    if not paths_config["PATH_OUTPUT"]:
+        icon.notify('PATH_OUTPUT is None. Enter it in settings!')
+    else:
+        return True
+
+
+def on_clicked_build(icon, item):
+    print("Build")
+    if not is_filepaths_exists(icon):
+        return
+    is_loading(icon, True)
+    print("Build: Start")
+    # updateModes()
+    is_loading(icon, False)
+    on_ready(icon)
+
+
+def on_clicked_build_and_start(icon, item):
+    print("Build And Start")
+    if not is_filepaths_exists(icon):
+        return
+    is_loading(icon, True)
+    print("Build And Start: Start")
+    # updateModesAndStart()
+    is_loading(icon, False)
+
+
+def on_clicked_exit(icon, item):
+    print("Exit")
+    icon.stop()
+
+
+def on_clicked_path_input(icon, item):
+    paths_config["PATH_INPUT"] = filedialog.askdirectory().replace("/", "\\")
+    paths.save()
+
+
+def on_clicked_path_output(icon, item):
+    paths_config["PATH_OUTPUT"] = filedialog.askdirectory().replace("/", "\\")
+    paths.save()
+
+
+def on_clicked_path_minecraft(icon, item):
+    paths_config["PATH_MINECRAFT"] = filedialog.askopenfilename().replace("/", "\\")
+    paths.save()
+
+
+def on_clicked_open_explorer(icon, item):
+    subprocess.Popen(rf'explorer "{paths_config["PATH_INPUT"]}"')
+    subprocess.Popen(rf'explorer "{paths_config["PATH_OUTPUT"]}"')
+
+
+def ui_menu():
+    return Menu(
+        MenuItem('Modpacks Build', action=None, enabled=False),
+        Menu.SEPARATOR,
+        MenuItem('Build', on_clicked_build, default=False),
+        MenuItem('Build and Start', on_clicked_build_and_start),
+        Menu.SEPARATOR,
+        MenuItem('Settings', Menu(
+            MenuItem("Path input", on_clicked_path_input),
+            MenuItem('Path output', on_clicked_path_output),
+            MenuItem('Path minecraft.exe', on_clicked_path_minecraft),
+            MenuItem('Open explorers', on_clicked_open_explorer),
+        )),
+        Menu.SEPARATOR,
+        MenuItem('Exit', on_clicked_exit),
+    )
+
+
+def on_error(icon, error):
+    icon.icon = image_error
+    icon.notify('ERROR:' + str(error))
+    print('ERROR:' + str(error))
+
+
+def main():
+    icon = Icon("Mods", image_main,
+                menu=ui_menu(),
+                title="Modpacks Build")
+    try:
+        icon.run()
+    except Exception as e:
+        on_error(icon, e)
+
+
+if __name__ == '__main__':
+    main()
